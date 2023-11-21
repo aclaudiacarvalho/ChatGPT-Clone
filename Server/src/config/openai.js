a1f2a7d4e686a9d96a11e910b43110b2c33b1f6e
aclaudiacarvalho <aclaudia_carvalho@outlook.com>
2023-11-20 21:30:11 -0300

    first commit

---
+++ Server/src/config/openai.js
@@ -0,0 +1,26 @@
+const { OpenAIApi } = require('openai')
+const { Configuration} = require('openai')
+require("dotenv").config()
+
+module.exports = class openai{
+
+	static configuration(){
+		const configuration = new Configuration({
+			apiKey: process.env.OPEN_AI_KEY,
+		})
+
+		return new OpenAIApi(configuration)
+	}
+
+	static textCompletion ({prompt}) {
+    return 	{
+			model:"text-davinci-003",
+			prompt:`${prompt}`,
+			temperature:0,
+			max_tokens: 3500,
+			top_p:1,
+			frequency_penalty: 0.5,
+			presence_penalty: 0
+		}
+  }
+}
